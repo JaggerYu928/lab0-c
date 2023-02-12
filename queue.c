c4022e22bf673d3cdbd03f8f07ce36eb57992a54
JaggerYu928 <jaggeryu928@gmail.com>
2023-02-12 16:21:15 +0000

    Completely understand how to write q_free

--- queue.c
+++ queue.c
@@ -14,31 +14,113 @@
 /* Create an empty queue */
 struct list_head *q_new()
 {
-    struct list_head *head = malloc(sizeof(struct list_head));
-    if (head)
-        INIT_LIST_HEAD(head);
-    return head;
+    struct list_head *new = malloc(sizeof(struct list_head));
+    if (!new)
+        return NULL;
+
+    INIT_LIST_HEAD(new);
+    return new;
 }
 
 /* Free all storage used by queue */
-void q_free(struct list_head *l) {}
+void q_free(struct list_head *l)
+{
+    if (!l)
+        return;
+
+    /*    struct list_head *node = l->next;
+        while (node != l) {
+            struct list_head *del = node;
+            node = node->next;
+            q_release_element(list_entry(del, element_t, list));
+        }
+        free(l);
+      */
+
+
+    element_t *node, *next;
+    list_for_each_entry_safe (node, next, l, list) {
+        list_del(&node->list);
+        q_release_element(node);
+    }
+    free(l);
+
+
+    /*    struct list_head *node;
+        list_for_each(node, l) {
+            struct list_head *del = node;
+            q_release_element(list_entry(del, element_t, list));
+        }
+        free(l);
+    */
+}
 
 /* Insert an element at head of queue */
 bool q_insert_head(struct list_head *head, char *s)
 {
+    if (!head)
+        return false;
+
+    element_t *new = malloc(sizeof(element_t));
+    if (!new)
+        return false;
+
+    INIT_LIST_HEAD(&new->list);
+    int str_size = strlen(s);
+    new->value = malloc((str_size + 1) * sizeof(char));
+
+    if (!new->value) {
+        free(new);
+        return false;
+    }
+
+    strncpy(new->value, s, str_size);
+    *(new->value + str_size) = '\0';
+    list_add(&new->list, head);
+
     return true;
 }
 
 /* Insert an element at tail of queue */
 bool q_insert_tail(struct list_head *head, char *s)
 {
+    if (!head)
+        return false;
+
+    element_t *new = malloc(sizeof(element_t));
+    if (!new)
+        return false;
+
+    INIT_LIST_HEAD(&new->list);
+    int str_size = strlen(s);
+    new->value = malloc((str_size + 1) * sizeof(char));
+
+    if (!new->value) {
+        free(new);
+        return false;
+    }
+
+    strncpy(new->value, s, str_size);
+    *(new->value + str_size) = '\0';
+    list_add_tail(&new->list, head);
+
     return true;
 }
 
 /* Remove an element from head of queue */
 element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head || list_empty(head))
+        return NULL;
+
+    element_t *target = list_entry(head->next, element_t, list);
+    list_del(&target->list);
+
+    if (bufsize) {
+        strncpy(sp, target->value, bufsize - 1);
+        *(sp + bufsize - 1) = '\0';
+    }
+    return target;
 }
 
 /* Remove an element from tail of queue */
